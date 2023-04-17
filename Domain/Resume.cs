43e5ab2362f784b21072a15d217c57f084cfd17a
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-04-16 18:20:15 -0700

    added inital migration

---
+++ Domain/Resume.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public class Resume
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
