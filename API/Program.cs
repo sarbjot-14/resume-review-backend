1dccd950d78f56c4aa95c85ba17889b0390c70cc
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-04-16 23:10:10 -0700

    added cors policy

--- API/Program.cs
+++ API/Program.cs
@@ -15,8 +15,12 @@ builder.Services.AddDbContext<DataContext>(opt=>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt=> opt.AddPolicy("CorsPolicy", policy=>{
+    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:3000");
+}));
 var app = builder.Build();
 
+app.UseCors("CorsPolicy");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
